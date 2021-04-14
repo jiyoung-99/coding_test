9d0670c8670a74c74c1baac81cac9bf2a4e0b9f5
jiyoung-99 <1skylar1111@gmail.com>
2021-04-14 14:22:02 +0900

    royal's night

--- Implementation/Royal's_night.py
+++ Implementation/Royal's_night.py
@@ -11,3 +11,27 @@
 여기서 위치를 입력 받고 이동 할 수 있는 경우의 수를 출력하여라
 
 """
+
+input_data = input()
+row = int(input_data[1])
+column = int(ord(input_data[0])-96)
+
+steps =[
+    (-2, -1), 
+    (-2, 1), 
+    (2, -1), 
+    (2, 1),
+    (1, -2), 
+    (1, 2), 
+    (-1, -2), 
+    (-1, 2),
+]
+
+result = 0
+for step in steps:
+    next_row = row+step[0]
+    next_column = column+step[1]
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <=8:
+        result += 1
+
+print(result)
